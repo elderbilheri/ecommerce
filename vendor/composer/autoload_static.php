463bb9374221ff885773b5b144d4ca84f6b41cd9
Elder Bilheri <elderbilheri@gmail.com>
2025-02-03 20:41:58 -0300

    criação da classe principal do banco

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit104ef3e2d08010f5233073719002b078
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/project/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInit104ef3e2d08010f5233073719002b078
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit104ef3e2d08010f5233073719002b078::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit104ef3e2d08010f5233073719002b078::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit104ef3e2d08010f5233073719002b078::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit104ef3e2d08010f5233073719002b078::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit104ef3e2d08010f5233073719002b078::$classMap;
 
